c71ce4432d76d173736b8dc562f7597be8aa0115
wajahat <wajahatakram414@gmail.com>
2021-05-18 02:31:00 +0500

    tansfering code to gamelogic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,55 +5,65 @@
       @board = (1..9).to_a 
     end
     def displayboard
-        puts '+---+---+---+'
-        puts '| 1 | 2 | 3 |'
-        puts '+---+---+---+'
-        puts '| 4 | 5 | 6 |'
-        puts '+---+---+---+'
-        puts '| 7 | 8 | 9 |'
-        puts '+---+---+---+'
-    end
-    def valid_move
-      input.between?(1, 9) && !position_taken?(input)
-    end
-    
-    def game_on?
-      num_turn < 9 && !win? ? true : false
+      puts '+---+---+---+'
+      puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+      puts '+---+---+---+'
+      puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+      puts '+---+---+---+'
+      puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+      puts '+---+---+---+'
     end
 
-    def draw?
-     !win && num_turn == 9
+    def checkwinner
+      if board[0]==board[1] && board[1]==board[2]
+        return true
+      elsif board[3]==board[4] && board[4]==board[5]
+        return true
+  
+      elsif board[6]==board[7] && board[7]==board[8]
+        return true
+  
+      elsif board[0]==board[3] && board[3]==board[6]
+        return true
+  
+      elsif board[1]==board[4] && board[4]==board[7]
+        return true
+  
+      elsif board[2]==board[5] && board[5]==board[8]
+        return true
+  
+      elsif board[0]==board[4] && board[4]==board[8]
+        return true
+  
+      elsif board[2]==board[4] && board[4]==board[6]
+        return true
+      else
+        return false
+      end
     end
 
-   def game_over?
-   end
+    def playerturn(name,x)
+      puts "Its #{name} turn!"
+      puts "\n"
+      puts 'Please select the available cell from the board'
+      cell_num = gets.chomp.to_i
+      loop do
+        if cell_num >= 1 && cell_num <= 9  &&  board[cell_num-1]==cell_num        
+          if x==0
+            board[cell_num-1]='X'
+          else
+            board[cell_num-1]='O'
+          end
+          break
+        end
+  
+        puts 'invalid move'
+        puts 'Please enter a valid number from 1 - 9!'
+        cell_num = gets.chomp.to_i
+      end
+    end
+  
 end
-
-#   WIN_COMBINATION_TEMP = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]].freeze
-
-#   def win?
-#     win_combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-#   end
-
-#   win_combination.each do |arr|
-#     win_arr1 = []
-#     win_arr2 = []
-#     arr.each do |x|
-#       player1.selections&.each do |option|
-#         win_arr1.push(x) if x == option
-#       end
-#       next if player2.selections.nil?
-
-#       player2.selections.each do |option|
-#         win_array2.push(x) if ix== option
-#       end
-#     end
-#     return true if win_arr1.length == 3 || win_arr2.length == 3
-#   end
-#   false
-# end
-# end
-
 
 
  
