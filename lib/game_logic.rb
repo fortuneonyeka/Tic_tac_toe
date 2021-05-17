9392c94979b5a4cf9798da46323952678a3f9859
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-17 20:31:25 +0100

    update win_combination

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -13,9 +13,6 @@
         puts '| 7 | 8 | 9 |'
         puts '+---+---+---+'
     end
-
-    WIN_COMBINATION_TEMP = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]].freeze
-
     def valid_move
       input.between?(1, 9) && !position_taken?(input)
     end
@@ -30,5 +27,33 @@
 
    def game_over?
    end
+
+
+  WIN_COMBINATION_TEMP = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]].freeze
+
+  def win?
+    win_combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  end
+
+  win_combination.each do |arr|
+    win_arr1 = []
+    win_arr2 = []
+    arr.each do |x|
+      player1.selections&.each do |option|
+        win_arr1.push(x) if x == option
+      end
+      next if player2.selections.nil?
+
+      player2.selections.each do |option|
+        win_array2.push(x) if ix== option
+      end
+    end
+    return true if win_arr1.length == 3 || win_arr2.length == 3
+  end
+  false
 end
+end
+
+
+
  
