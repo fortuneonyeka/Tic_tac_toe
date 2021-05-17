f6dc0b38809a519ee54c3c281899f788f2418944
wajahat <wajahatakram414@gmail.com>
2021-05-17 22:49:46 +0500

    completed players turn and valid input

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,24 +1,32 @@
-class Board
-      private
-    @@board
+ class Board
     public
+    attr_accessor :board
     def initialize
-      @@board = Array.new(3) { Array.new(3) { 0 } }
+      @board = (1..9).to_a 
     end
-end
+    def displayboard
+        puts '+---+---+---+'
+        puts '| 1 | 2 | 3 |'
+        puts '+---+---+---+'
+        puts '| 4 | 5 | 6 |'
+        puts '+---+---+---+'
+        puts '| 7 | 8 | 9 |'
+        puts '+---+---+---+'
+    end
 
-def valid_move
-  input.between?(1, 9) && !position_taken?(input)
-end
+    def valid_move
+      input.between?(1, 9) && !position_taken?(input)
+    end
+    
+    def game_on?
+      num_turn < 9 && !win? ? true : false
+    end
 
-def game_on?
-  num_turn < 9 && !win? ? true : false
-end
+    def draw?
+     !win && num_turn == 9
+    end
 
-def draw?
-  !win && num_turn == 9
+   def game_over?
+   end
 end
-
-def game_over?
-  draw? || win? || num_turn == 9?
-end
+ 
