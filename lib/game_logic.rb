34b06ecfa344e2f25309c6b688f7211b6b50eedb
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-18 18:47:53 +0100

    refactor diplayboard on the game_logic and on main

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -9,15 +9,15 @@ class Board
     @board = (1..9).to_a
   end
 
-  def displayboard
-    puts '+---+---+---+'
-    puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
-    puts '+---+---+---+'
-    puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
-    puts '+---+---+---+'
-    puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
-    puts '+---+---+---+'
-  end
+  def display_board
+  
+    " #{board[0]} | #{board[1]} | #{board[2]} \n" \
+    "+---+---+---+\n" \
+    " #{board[3]} | #{board[4]} | #{board[5]} \n" \
+    "+---+---+---+\n" \
+    " #{board[6]} | #{board[7]} | #{board[8]} \n" \
+   
+ end
 
   def checkwinner
     return true if board[0] == board[1] && board[1] == board[2]
@@ -39,20 +39,22 @@ class Board
     false
   end
 
-  def playerturn(cell_num, turn)
-    loop do
-      if cell_num >= 1 && cell_num <= 9 && board[cell_num - 1] == cell_num
-        board[cell_num - 1] = if turn.zero?
-                                'X'
-                              else
-                                'O'
-                              end
-        break
-      end
-      puts 'invalid move Please enter a valid number from 1 - 9!'
-      cell_num = gets.chomp.to_i
+
+def playerturn(cell_num, turn)
+  loop do
+    if cell_num >= 1 && cell_num <= 9 && board[cell_num - 1] == cell_num
+      board[cell_num - 1] = if turn.zero?
+                              'X'
+                            else
+                              'O'
+                            end
+      break
     end
+    puts 'invalid move Please enter a valid number from 1 - 9!'
+    cell_num = gets.chomp.to_i
   end
 end
+end
+  
 
 # rubocop: enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity,  Metrics/AbcSize
