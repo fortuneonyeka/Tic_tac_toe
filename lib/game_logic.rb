8ca67bb28c56712be30803ed20cdc2d2248778f0
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-22 01:18:35 +0100

    update checkwinner false and checkwinner true and also tie

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -18,25 +18,17 @@ class Board
   end
 
   def checkwinner
-    return true if board[0] == board[1] && board[1] == board[2]
-
-    return true if board[3] == board[4] && board[4] == board[5]
-
-    return true if board[6] == board[7] && board[7] == board[8]
-
-    return true if board[0] == board[3] && board[3] == board[6]
-
-    return true if board[1] == board[4] && board[4] == board[7]
-
-    return true if board[2] == board[5] && board[5] == board[8]
-
-    return true if board[0] == board[4] && board[4] == board[8]
-
-    return true if board[2] == board[4] && board[4] == board[6]
-
-    false
+    win_combo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    win_combo.any? do |indexes|
+     [%w[X X X],%w[O O O]].include? indexes.map {|i|board[i]}
+    end
+  end
+  
+  def tie?
+    num_turn == 9 && !win?
   end
 
+  
   def playerturn(cell_num, turn)
     if cell_num >= 1 && cell_num <= 9 && board[cell_num - 1] == cell_num
       board[cell_num - 1] = if turn == 0
--- spec/game_logic_rspec.rb
+++ spec/game_logic_rspec.rb
@@ -1,10 +1,9 @@
 require './lib/game_logic'
 
 
-
   describe Board do
+    let(:play) {Board.new }
     describe '#display_board' do
-      play = Board.new 
       it 'should display number from 1-9' do
         expect(play.display_board).to eql(
           " 1 | 2 | 3 \n" \
@@ -14,13 +13,58 @@ require './lib/game_logic'
           " 7 | 8 | 9 \n"
         )
       end
-  end
+   end
 
-  describe '#player_turn' do
-    play = Board.new 
+   describe '#player_turn' do
     it 'should display available options' do
       expect(@board = (1..9).to_a).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
+   end
+
+   describe '#chekwinner' do 
+    it 'should return false' do
+      expect(play.checkwinner).to eq(false)
+    end
+
+   it 'First player should return true' do
+    play.playerturn(1, 0)
+    play.playerturn(2, 0)
+    play.playerturn(3, 0)
+     expect(play.checkwinner).to eq(true)
+   end
+   it 'Second player should return true' do
+    play.playerturn(1, 1)
+    play.playerturn(4, 1)
+    play.playerturn(7, 1)
+     expect(play.checkwinner).to eq(true)
+   end
+  describe '#tie?' do
+    num_turn = 0
+    it 'should display true if all board position is taken' do
+      play.playerturn(0, 'X')
+      play.num_turn
+      play.playerturn(1, 'O')
+      play.num_turn
+      play.playerturn(2, 'X')
+      play.num_turn
+      play.playerturn(3, 'O')
+      play.num_turn
+      play.playerturn(4, 'X')
+      play.num_turn
+      play.playerturn(5, 'O')
+      play.num_turn
+      play.playerturn(6, 'O')
+      play.num_turn
+      play.playerturn(7, 'X')
+      play.num_turn
+      play.playerturn(8, 'O')
+      play.num_turn
+      play.num_turn
+      play.cell_num
+      expect(play.tie?).to eql(true)
+      num_turn +=1
+    end
   end
+   
 end
-  
+end
